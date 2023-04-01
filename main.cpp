28ac10cefdfc0cfa0a09e551e47f0d4a4285e15b
Stephan Bruny <stephan.bruny@live.de>
2023-04-01 12:14:47 +0200

    Textured Raycaster!

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ using namespace std;
 constexpr int WINDOW_WIDTH = 1280;
 constexpr int WINDOW_HEIGHT = 800;
 
-constexpr int MAP_WIDTH = 10;
-constexpr int CELL_SIZE = 32;
+constexpr int MAP_WIDTH = 30;
+constexpr int CELL_SIZE = 8;
 constexpr float PI_DIV2 = M_PI / 2;
 constexpr float PI_3 = 3*PI_DIV2;
 constexpr float PI_2 = 2*M_PI;
@@ -25,30 +25,55 @@ inline float degreeToRadians(float degree) {
 }
 
 vector<int> tilemap = {
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 0, 0, 0, 0, 1, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 1, 0, 0, 0, 0, 0, 0, 1, 1,
-    1, 1, 0, 0, 0, 0, 0, 0, 1, 1,
-    1, 0, 1, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 1, 0, 1,
-    1, 0, 0, 0, 1, 1, 0, 0, 0, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    13,13,2, 3, 4, 13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13,13,13,13,13,13,13,0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13,0, 0, 0, 0, 0, 13,0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13,0, 0, 0, 0, 0, 13,0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13,0, 0, 0, 0, 0, 13,0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13,0, 0, 0, 0, 0, 13,0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13,13,13,13,13,13,13,0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,1
 };
 
 struct Player {
     float rotation;
     Vector2 position;
     Vector2 delta;
+    Vector2 direction;
+    Vector2 plane;
+    shared_ptr<vector<int>> map;
 
-    Player(float x = 0, float y = 0) {
+    Player(shared_ptr<vector<int>> map) {
+        this->map = map;
         this->rotation = 0.0f;
-        this->position = Vector2 { x, y };
+        this->position = Vector2 { 0, 0 };
+        this->direction = Vector2 { -1, 0 };
+        this->plane = Vector2 { 0, 0.66 };
         this->delta = Vector2 { 0, 0 };
     }
 
-    void rotate(float amount = 0.1f, float deltaAmount = 1.0f) {
+    void rotate(float amount = 0.1f, float deltaAmount = 0.1f) {
         this->rotation += amount;
 
         if (this->rotation < 0) this->rotation = MAX_ROTATION;
@@ -56,16 +81,33 @@ struct Player {
 
         this->delta.x = cos(degreeToRadians(this->rotation)) * deltaAmount;
         this->delta.y = sin(degreeToRadians(this->rotation)) * deltaAmount;
+
+        auto oldDirX = this->direction.x;
+        this->direction.x = this->direction.x * cos(-amount) - this->direction.y * sin(-amount);
+        this->direction.y = oldDirX * sin(-amount) + this->direction.y * cos(-amount);
+        auto oldPlaneX = this->plane.x;
+        this->plane.x = this->plane.x * cos(-amount) - this->plane.y * sin(-amount);
+        this->plane.y = oldPlaneX * sin(-amount) + this->plane.y * cos(-amount);
     }
 
-    void moveForward() {
-        this->position.x += this->delta.x;
-        this->position.y += this->delta.y;
+    void moveForward(float amount = 0.1f) {
+        int px = (this->position.x + this->direction.x * 1.0);
+        int py = (this->position.y + this->direction.y * 1.0);
+        int index = py * MAP_WIDTH + px;
+        if ((*this->map)[index] > 0) return;
+
+        this->position.x += this->direction.x * amount;
+        this->position.y += this->direction.y * amount;
     }
 
-    void moveBackward() {
-        this->position.x -= this->delta.x;
-        this->position.y -= this->delta.y;
+    void moveBackward(float amount = 0.1f) {
+        int px = (this->position.x - this->direction.x * 1.0);
+        int py = (this->position.y - this->direction.y * 1.0);
+        int index = py * MAP_WIDTH + px;
+        if ((*this->map)[index] > 0) return;
+
+        this->position.x -= this->direction.x * amount;
+        this->position.y -= this->direction.y * amount;
     }
 };
 
@@ -73,69 +115,125 @@ float getDistance(Vector2 a, Vector2 b, float ang) {
     return sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y));
 }
 
-void castRays(Player &player, vector<int> map, int maxDistance = 1000, int mapWidth = 10, int fov = 60) {
-    int halfScreenWidth = Config::DISPLAY_WIDTH / 2;
-    int halfScreenHeight = Config::DISPLAY_HEIGHT / 2;
-    int halfFov = fov / 2;
-    float incrementAngle = (float)fov / Config::DISPLAY_WIDTH;
-    float rayAngle = (float)player.rotation - (float)halfFov;
-    for(int rayCount = 0; rayCount < Config::DISPLAY_WIDTH; rayCount++) {
-        if (rayAngle < 0) rayAngle = 360.0f + rayAngle;
-        if (rayAngle > 360.0f) rayAngle = rayAngle - 360.0f;
-        float rad = degreeToRadians(rayAngle);
-        Vector2 ray { player.position.x, player.position.y };
-        float rayCos = cos(rad) / CELL_SIZE;
-        float raySin = sin(rad) / CELL_SIZE;
-        int wallDistance = 0;
+void renderRaycaster(Player &player, vector<int> &map, int mapWidth, Texture2D &textures) {
+    for (int x = 0; x < Config::DISPLAY_WIDTH; x++) {
+        double cameraX = 2 * x / double(Config::DISPLAY_WIDTH) - 1; //x-coordinate in camera space
+        double rayDirX = player.direction.x + player.plane.x * cameraX;
+        double rayDirY = player.direction.y + player.plane.y * cameraX;
 
-        while (wallDistance < maxDistance) {
-            ray.x += rayCos;
-            ray.y += raySin;
-            int x = (int)ray.x / CELL_SIZE;
-            int y = (int)ray.y / CELL_SIZE;
-            int wallIndex = y * mapWidth + x;
-            if (wallIndex >= 0 && wallIndex < map.size() && map[wallIndex] > 0) {
-                break;
-            }
-            wallDistance++;
+        //which box of the map we're in
+        int mapX = int(player.position.x);
+        int mapY = int(player.position.y);
+
+        //length of ray from current position to next x or y-side
+        double sideDistX;
+        double sideDistY;
+
+        //length of ray from one x or y-side to next x or y-side
+        double deltaDistX = (rayDirX == 0) ? 1e30 : std::abs(1 / rayDirX);
+        double deltaDistY = (rayDirY == 0) ? 1e30 : std::abs(1 / rayDirY);
+        double perpWallDist;
+
+        //what direction to step in x or y-direction (either +1 or -1)
+        int stepX;
+        int stepY;
+
+        int hit = 0; //was there a wall hit?
+        int side; //was a NS or a EW wall hit?
+
+        if (rayDirX < 0) {
+            stepX = -1;
+            sideDistX = (player.position.x - mapX) * deltaDistX;
+        } else {
+            stepX = 1;
+            sideDistX = (mapX + 1.0 - player.position.x) * deltaDistX;
+        }
+        if (rayDirY < 0) {
+            stepY = -1;
+            sideDistY = (player.position.y - mapY) * deltaDistY;
+        } else
+        {
+            stepY = 1;
+            sideDistY = (mapY + 1.0 - player.position.y) * deltaDistY;
         }
 
-        /*
-        float px = player.position.x - ray.x;
-        float py = player.position.y - ray.y;
-        auto distance = (float)::sqrt(
-                (float)::pow(px, 2) + (float)::pow(py, 2)
-        );*/
+        int rayDepth = 0;
+        int wallTextureId = -1;
+        while (rayDepth < 10000)
+        {
+            //jump to next map square, either in x-direction, or in y-direction
+            if (sideDistX < sideDistY) {
+                sideDistX += deltaDistX;
+                mapX += stepX;
+                side = 0;
+            } else {
+                sideDistY += deltaDistY;
+                mapY += stepY;
+                side = 1;
+            }
+            int mapIndex = mapY * mapWidth + mapX;
+            //Check if ray has hit a wall
+            if (mapIndex >= 0 && mapIndex < map.size() && map[mapIndex] > 0) {
+                wallTextureId = map[mapIndex];
+                break;
+            }
+            rayDepth++;
+        }
 
-        float distance = getDistance(player.position, ray, rad);
-        unsigned char depth = 255;
-        depth = (1 / distance) * 255;
-        if (depth < 0) depth = 0;
-        if (depth > 255) depth = 255;
-        distance = distance * (float)cos(degreeToRadians(rayAngle - player.rotation));
+        //calculate value of wallX
+        double wallX; //where exactly the wall was hit
+        if (side == 0) wallX = player.position.y + perpWallDist * rayDirY;
+        else           wallX = player.position.x + perpWallDist * rayDirX;
+        wallX -= floor((wallX));
 
-        int wallHeight = (int)::floor((float)halfScreenHeight / distance);
-        if (wallHeight < 0) wallHeight = 0;
-        if (wallHeight > Config::DISPLAY_HEIGHT) wallHeight = Config::DISPLAY_HEIGHT;
-        Color color = { depth, depth, depth, 255 };
-        if (distance > 0)
-            DrawLine(
-                rayCount,
-                halfScreenHeight - wallHeight,
-                rayCount,
-                halfScreenHeight + wallHeight,
+        //x coordinate on the texture
+        int texX = int(wallX * double(Config::TEXTURE_SIZE));
+        if(side == 0 && rayDirX > 0) texX = Config::TEXTURE_SIZE - texX - 1;
+        if(side == 1 && rayDirY < 0) texX = Config::TEXTURE_SIZE - texX - 1;
+
+        if(side == 0)
+            perpWallDist = (sideDistX - deltaDistX);
+        else
+            perpWallDist = (sideDistY - deltaDistY);
+
+        //Calculate height of line to draw on screen
+        int lineHeight = (int)(Config::DISPLAY_HEIGHT / perpWallDist);
+
+        //calculate lowest and highest pixel to fill in current stripe
+        double wallLightDist = perpWallDist;
+        if (wallLightDist < 1) wallLightDist = 1;
+        unsigned char wallDepth = (1 / wallLightDist) * ((side == 1) ? 128 : 255);
+        int drawStart = -lineHeight / 2 + Config::DISPLAY_HEIGHT / 2;
+        // if(drawStart < 0)drawStart = 0;
+        int drawEnd = lineHeight / 2 + Config::DISPLAY_HEIGHT / 2;
+        // if(drawEnd >= Config::DISPLAY_HEIGHT)drawEnd = Config::DISPLAY_HEIGHT - 1;
+
+        Color color = WHITE; // { wallDepth, wallDepth, wallDepth, 255 };
+        if (side == 1) color = GRAY;
+
+        // DrawLine(x, drawStart, x, drawEnd, color);
+        int atlasWidth = textures.width / Config::TEXTURE_SIZE;
+        int textureIdX = wallTextureId % atlasWidth;
+        int textureIdY = wallTextureId / atlasWidth;
+        float textureX = textureIdX * Config::TEXTURE_SIZE + texX;
+        float textureY = textureIdY * Config::TEXTURE_SIZE;
+        DrawTexturePro(
+                textures,
+                Rectangle { textureX, textureY, 1, Config::TEXTURE_SIZE },
+                Rectangle { (float)x, (float)drawStart, 1, float(drawEnd - drawStart) },
+                Vector2 { 0, 0 },
+                0.0,
                 color
-            );
-        rayAngle += incrementAngle;
+        );
     }
 }
 
 void update(double dt, Player& player) {
     if (IsKeyDown(KEY_LEFT)) {
-        player.rotate(-1.0f);
+        player.rotate(-0.10f);
     }
     if (IsKeyDown(KEY_RIGHT)) {
-        player.rotate(1.0f);
+        player.rotate(0.10f);
     }
     if (IsKeyDown(KEY_UP)) {
         player.moveForward();
@@ -145,9 +243,9 @@ void update(double dt, Player& player) {
     }
 }
 
-void render(Player &player) {
+void render(Player &player, Texture2D &textures) {
     ClearBackground(BLACK);
-    castRays(player, tilemap);
+    renderRaycaster(player, tilemap, MAP_WIDTH, textures);
 }
 
 void renderMinimap(Player &player, vector<int> &tilemap, int mapWidth, Vector2 offset = { WINDOW_WIDTH / 2, 0 }) {
@@ -165,7 +263,7 @@ void renderMinimap(Player &player, vector<int> &tilemap, int mapWidth, Vector2 o
         }
     }
 
-    DrawCircle(player.position.x + offset.x, player.position.y + offset.y, 8, playerColor);
+    DrawCircle(player.position.x * CELL_SIZE + offset.x, player.position.y * CELL_SIZE + offset.y, 8, playerColor);
 }
 
 int main() {
@@ -175,14 +273,17 @@ int main() {
     SetTargetFPS(60);
 
     double currentTime = 0.0;
+    auto shared_map = make_shared<vector<int>>(tilemap);
 
-    Player player;
-    player.position = { 5 * CELL_SIZE, 5 * CELL_SIZE };
+    Player player(shared_map);
+    player.position = { 5, 5 };
     player.rotation = 180;
 
     RenderTexture2D canvas = LoadRenderTexture(Config::DISPLAY_WIDTH, Config::DISPLAY_HEIGHT);
     Rectangle canvasSource = { 0, 0, Config::DISPLAY_WIDTH, Config::DISPLAY_HEIGHT };
     Rectangle canvasDest = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+
+    Texture2D textures = LoadTexture("assets/textures.png");
 
     while (!WindowShouldClose())
     {
@@ -191,15 +292,18 @@ int main() {
         currentTime = time;
 
         BeginTextureMode(canvas);
-            render(player);
+            render(player, textures);
         EndTextureMode();
 
         BeginDrawing();
             ClearBackground(BLACK);
             DrawTexturePro(canvas.texture, canvasSource, canvasDest, Vector2 { 0, 0 }, 0, WHITE );
-            renderMinimap(player, tilemap, 10);
+            renderMinimap(player, tilemap, MAP_WIDTH);
         EndDrawing();
     }
+
+    UnloadRenderTexture(canvas);
+    UnloadTexture(textures);
 
     CloseWindow();
 
